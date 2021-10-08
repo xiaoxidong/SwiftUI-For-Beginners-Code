495b9b6461855d7ae23fe0425e9c3259862006a8
xiaodong <834599524@qq.com>
2021-10-08 13:18:35 +0800

    Init project

---
+++ Shared/SwiftUI_For_Designers_CodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_For_Designers_CodeApp.swift
+//  Shared
+//
+//  Created by XiaoDong Yuan on 2021/10/8.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_For_Designers_CodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
