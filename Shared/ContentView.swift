506ec392b7a81dced5b7c9e98ef7c09d89d90915
xiaodong <834599524@qq.com>
2021-10-19 20:41:19 +0800

    增加 SwiftUIBug 项目

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by XiaoDong Yuan on 2021/10/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/SwiftUIBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBugApp.swift
+//  Shared
+//
+//  Created by XiaoDong Yuan on 2021/10/19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
